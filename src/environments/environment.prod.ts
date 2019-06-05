53eb165f238fb68da3402199b6f98a9f2b9ea0ad
Tony <tomcatbuzz@yahoo.com>
2019-06-05 02:41:59 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBWZJY84XKz17GlZjbHKn6IHmHoOL_jcN0',
-    authDomain: 'angular-pwa-26994.firebaseapp.com',
-    databaseURL: 'https://angular-pwa-26994.firebaseio.com',
-    projectId: 'angular-pwa-26994',
-    storageBucket: 'angular-pwa-26994.appspot.com',
-    messagingSenderId: '158700649043'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: 'https://.firebaseio.com',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
